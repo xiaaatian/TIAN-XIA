555c056ff32d08b253d96060d5b3c44da563b36b
xiaaatian <fishingthesoundxt@gmail.com>
2025-02-05 16:32:24 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,27 @@
-// 图片列表
+// 图片数组
 const images = [
-    "images/plantasia1.jpg",
-    "images/plantasia2.jpg",
-    "images/plantasia3.jpg",
-    "images/plantasia4.jpg"
+    'images/plantasia1.jpg',
+    'images/plantasia2.jpg',
+    'images/plantasia3.jpg',
 ];
 
-// 当前显示的图片索引
+// 当前图片索引
 let currentIndex = 0;
 
 // 获取图片元素
-const galleryImage = document.getElementById("gallery-image");
+const slideshowImage = document.getElementById('slideshow-image');
 
-// 切换图片函数
-function changeImage(direction) {
-    // 更新索引
+// 改变图片
+function changeSlide(direction) {
     currentIndex += direction;
 
-    // 循环图片索引
+    // 循环显示图片
     if (currentIndex < 0) {
-        currentIndex = images.length - 1; // 回到最后一张
+        currentIndex = images.length - 1;
     } else if (currentIndex >= images.length) {
-        currentIndex = 0; // 回到第一张
+        currentIndex = 0;
     }
 
-    // 更新图片 src
-    galleryImage.src = images[currentIndex];
+    // 更新图片源
+    slideshowImage.src = images[currentIndex];
 }
