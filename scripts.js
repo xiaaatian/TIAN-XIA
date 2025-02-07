f26ff42e5d6052ac697531bb78ce2e49caa80343
xiaaatian <fishingthesoundxt@gmail.com>
2025-02-07 00:26:14 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,18 +1,17 @@
-// 获取按钮和覆盖页面
-const overlay = document.querySelector(".overlay");
-const toggleBtn = document.querySelector(".toggle-overlay-btn");
-const closeBtn = document.querySelector(".close-overlay-btn");
+document.addEventListener("DOMContentLoaded", function () {
+    const overlay = document.querySelector(".overlay");
+    const toggleBtn = document.querySelector(".toggle-overlay-btn");
+    const closeBtn = document.querySelector(".close-overlay-btn");
 
-// 打开覆盖页面
-if (toggleBtn) {
-    toggleBtn.addEventListener("click", () => {
-        overlay.style.display = "flex";
-    });
-}
+    if (toggleBtn && closeBtn && overlay) {
+        // 打开覆盖页面
+        toggleBtn.addEventListener("click", () => {
+            overlay.style.display = "flex";
+        });
 
-// 关闭覆盖页面
-if (closeBtn) {
-    closeBtn.addEventListener("click", () => {
-        overlay.style.display = "none";
-    });
-}
+        // 关闭覆盖页面
+        closeBtn.addEventListener("click", () => {
+            overlay.style.display = "none";
+        });
+    }
+});
